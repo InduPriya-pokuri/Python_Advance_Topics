4dca057a1f8eec9bf4ee31799d7f71fd0cbead48
InduPriya-pokuri <34738280+InduPriya-pokuri@users.noreply.github.com>
2020-02-20 05:36:58 +0530

    Add files via upload

---
+++ list comprehension.py
@@ -0,0 +1,7 @@
+''' intializing and storing values in list
+    range() of numbers
+    taking input from user
+    stored the even
+    multiplication table
+    ch:ord(ch)=even store the characters
+    if numbers are even store their characters from the user
